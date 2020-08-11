21785158aa931de4137f9646f156f2b9841569b4
Bishal Deb <68964445+Bishal-Deb@users.noreply.github.com>
2020-08-11 10:09:19 +0530

    Lab Class
    change updates

--- lab1.java
+++ lab1.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
+
+
 public class lab1 {
    public static void main(String args[]) {
       Scanner input = new Scanner(System.in);
+      Scanner stdin = new Scanner(System.in);
       String array[] = new String[5];
-      for (int i = 0 ; i < 5; i++ ) {
+
+       System.out.println("Want to add new order Details for sure? (y/n)");
+
+      if (stdin.next().startsWith("y")){
+      	for (int i = 0 ; i < 5; i++ ) {
            System.out.println("Enter the details fot the new order: ");
            array[i] = input.next();
         }
-     System.out.println("New order details: ");
+        
+        System.out.println("New order details: ");
+      }
+
         printArray(array);
 
         input.close();
+        stdin.close();
    }
    public static void printArray(String arr[]){
 
